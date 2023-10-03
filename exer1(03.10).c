11b4a6eadf6a36a3031dbdcbf2b84a87f8588b3b
Maria <142902000+MariaSelhorst@users.noreply.github.com>
2023-10-03 11:16:45 -0300

    Add files via upload

---
+++ exer1(03.10).c
@@ -0,0 +1,24 @@
+/*1- Leia um array com 10 posições e identifique se existem valores iguais neste array. se existirem, envie para um outro array e o exiba na tela.*/
+
+#include <stdio.h>
+
+
+int main()
+{
+    int array[10] = {1,2,3,4,3,8,7,6,6,8};
+    int array2[10];
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+           if (array[i] == array2[j])
+           {
+            
+           } 
+        }
+        
+    }
+ 
+}
+
